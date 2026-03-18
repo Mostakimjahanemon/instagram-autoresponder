0773bdcab9384af30067a7fa1c5800b90596f2c4
Mostakimjahanemon <155299221+Mostakimjahanemon@users.noreply.github.com>
2026-03-18 17:02:05 +0600

    Update run.py

--- run.py
+++ run.py
@@ -66,5 +66,5 @@ def home():
     return 'Instagram Bot is Running! 🤖'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
